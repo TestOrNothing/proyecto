4dab8bc72ed59164de7a92043fe7ec368a17cb1a
vicentegonz <vgonzalez4@uc.cl>
2022-10-28 20:11:29 -0300

    son 4

--- test/models/movie_time_test.rb
+++ test/models/movie_time_test.rb
@@ -3,4 +3,15 @@
 require 'test_helper'
 
 class MovieTimeTest < ActiveSupport::TestCase
+    def setup
+        @bad_movie = Movie.create(title: 'Matrix')
+      end
+
+    test 'start_date > end_date' do 
+        movietime = MovieTime.new(room: 5, date_start: Date.new(2001, 10, 10),
+          date_end: Date.new(2000, 11, 12), time: 'TANDA',
+          movie_id: @bad_movie.id)
+          movietime.validate_date
+          assert_equal('El día de inicio tiene que ser antes o igual al día de termino', movietime.errors.messages[:date_start][0])
+      end
 end
