f4fb32222fa6b7658ab40e26d6e74d3f84194e88
vicentegonz <vgonzalez4@uc.cl>
2022-11-10 00:55:19 -0300

    model fix

--- app/models/movie_time.rb
+++ app/models/movie_time.rb
@@ -24,9 +24,9 @@ class MovieTime < ApplicationRecord
                  'El día de inicio tiene que ser antes o igual al día de termino')
     end
     # rubocop:disable Layout/LineLength
-    query_text = '((? <= date_end and ? >= date_start) or (? <= date_end and ? >= date_start)) and time = ? and room = ?'
+    query_text = '((? <= date_end and ? >= date_start) or (? <= date_end and ? >= date_start)) and time = ? and room = ? and location = ?'
     # rubocop:enable Layout/LineLength
-    query = MovieTime.where([query_text, date_start, date_start, date_end, date_end, time, room])
+    query = MovieTime.where([query_text, date_start, date_start, date_end, date_end, time, room, location])
     # rubocop:disable Style/GuardClause
     if query.length.positive?
       errors.add(
